4e8abe8c8456171cfe4e274904f8e84f98aabb0e
iirtrtrt <tk@tk>
2023-03-02 15:19:53 +0200

    change google analytics ID

--- app/head.tsx
+++ app/head.tsx
@@ -7,13 +7,13 @@ export default function Head() {
       <link rel="icon" href="/favicon.ico" />
       <script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-38LSCTYW08"
+        src="https://www.googletagmanager.com/gtag/js?id=G-WRRVC7NXPP"
       ></script>
       <script>
         {` window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-38LSCTYW08');`}
+  gtag('config', 'G-WRRVC7NXPP');`}
       </script>
     </>
   );
