3d1f792c04f460cd67a4b26e938393479daeee0d
iirtrtrt <tk@tk>
2023-03-02 15:06:58 +0200

    add google analytics tag

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,16 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="This is a Taekyung Kim's web" />
       <link rel="icon" href="/favicon.ico" />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-38LSCTYW08"
+      ></script>
+      <script>
+        {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-38LSCTYW08');`}
+      </script>
     </>
   );
 }
