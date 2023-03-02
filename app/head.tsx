910c42bf9ca73943f0cdd374c37f51aa5f3f32ff
iirtrtrt <tk@tk>
2023-03-02 20:10:34 +0200

    GA works

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
@@ -5,6 +7,16 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="This is from Taekyung Kim" />
       <link rel="icon" href="/favicon.ico" />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-WRRVC7NXPP"
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-WRRVC7NXPP');`}
+      </Script>
     </>
   );
 }
