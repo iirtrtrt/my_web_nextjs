4f82e5d52520447439e0afc9a88bdcacb76e8f19
iirtrtrt <tk@tk>
2023-03-07 23:57:19 +0200

    add icons and preview image:

--- app/head.tsx
+++ app/head.tsx
@@ -6,7 +6,25 @@ export default function Head() {
       <title>TKINLV</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="This is from Taekyung Kim" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta property="image" content="/preview-tkinlv.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-WRRVC7NXPP"
